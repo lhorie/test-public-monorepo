bee346c69f3cf465af9b25e4f0a4d1ece23192b1
Leo Horie <lhorie@uber.com>
2019-01-28 17:12:42 -0800

    import fusion-plugin-i18n-react

---
+++ fusion-plugin-i18n-react/src/index.js
@@ -0,0 +1,20 @@
+/** Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+import I18n from './plugin';
+
+export {
+  I18nToken,
+  I18nLoaderToken,
+  HydrationStateToken,
+  createI18nLoader,
+} from 'fusion-plugin-i18n';
+export {withTranslations} from './with-translations';
+export {Translate} from './translate';
+
+export default I18n;

b9df0b8c82eaa9c928f0164421cec0b301f0ab9c
Leo Horie <lhorie@uber.com>
2019-01-28 16:03:47 -0800

    Import fusion-plugin-react-redux

---
+++ fusion-plugin-react-redux/src/tokens.js
@@ -0,0 +1,28 @@
+/** Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+import type {Reducer, StoreEnhancer} from 'redux';
+
+import {createToken} from 'fusion-core';
+import type {Token} from 'fusion-core';
+
+import type {GetInitialStateType, ReactReduxServiceType} from './types.js';
+
+export const ReduxToken: Token<ReactReduxServiceType> = createToken(
+  'ReduxToken'
+);
+export const ReducerToken: Token<Reducer<*, *>> = createToken('ReducerToken');
+export const PreloadedStateToken: Token<Object> = createToken(
+  'PreloadedStateToken'
+);
+export const EnhancerToken: Token<StoreEnhancer<*, *, *>> = createToken(
+  'EnhancerToken'
+);
+export const GetInitialStateToken: Token<
+  GetInitialStateType<Object>
+> = createToken('GetInitialStateToken');

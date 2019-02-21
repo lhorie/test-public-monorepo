fedd389d6951ff1847ef50590768befc68ffb48b
Leo Horie <lhorie@uber.com>
2019-02-20 17:12:24 -0800

    Import fusion-plugin-http-handler

---
+++ fusion-plugin-http-handler/src/server.js
@@ -0,0 +1,77 @@
+/** Copyright (c) 2018 Uber Technologies, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+/* eslint-env node */
+
+import {createPlugin} from 'fusion-core';
+import type {FusionPlugin} from 'fusion-core';
+import {HttpHandlerToken} from './tokens.js';
+import type {DepsType} from './types.js';
+
+const plugin =
+  __NODE__ &&
+  createPlugin({
+    deps: {
+      handler: HttpHandlerToken,
+    },
+
+    middleware: deps => {
+      const {handler} = deps;
+      return async (ctx, next) => {
+        await next();
+        if (ctx.body) {
+          return;
+        }
+        return new Promise((resolve, reject) => {
+          const {req, res} = ctx;
+          // Default http response object behavior defaults res.statusCode to 200. Koa sets it to 404.
+          // This allows for http servers to use `end()` or express to use `send()` without specifying a 200 status code
+          const prevStatusCode = ctx.res.statusCode;
+          ctx.res.statusCode = 200;
+          const listener = () => {
+            // Makes koa-bodyparser compatible with bodyparser
+            // $FlowFixMe
+            req._body = true;
+            // $FlowFixMe
+            req.body = ctx.request.body;
+            // $FlowFixMe
+            res.setHeader = () => {};
+            ctx.respond = false;
+            done(null);
+          };
+          res.on('end', listener);
+          res.on('finish', listener);
+
+          handler(req, res, error => {
+            ctx.res.statusCode = prevStatusCode;
+            // $FlowFixMe
+            return done(error);
+          });
+
+          function done(error) {
+            // Express mutates the req object to make this property non-writable.
+            // We need to make it writable because other plugins (like koa-helmet) will set it
+            // $FlowFixMe
+            Object.defineProperty(req, 'secure', {
+              // $FlowFixMe
+              value: req.secure,
+              writable: true,
+            });
+            res.removeListener('end', listener);
+            res.removeListener('finish', listener);
+            if (error) {
+              return reject(error);
+            }
+            return resolve();
+          }
+        });
+      };
+    },
+  });
+
+export default ((plugin: any): FusionPlugin<DepsType, void>);
